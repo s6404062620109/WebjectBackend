0a4012872f355dfc37ced890a612661965390ca4
unknown <s6404062620109@email.kmutnb.ac.th>
2023-10-12 19:49:38 +0700

    add postproductid, getcart, getAllproduct, deleteProduct api.

--- index.js
+++ index.js
@@ -24,19 +24,6 @@ const db = mysql.createConnection({
     database: "webject"
 })
 
-
-
-app.get('/users', (req, res) =>{
-    db.query("SELECT * FROM user", (err, result) =>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.send(result);
-        }
-    });
-});
-
 app.get('/bestSell', (req, res) =>{
     db.query("SELECT product_history.productid, product.name, product.picture, SUM(product_history.quantity) as total_quantity FROM product_history JOIN product ON product_history.productid = product.productid GROUP BY product_history.productid ORDER BY total_quantity DESC", 
     (err, result) =>{
@@ -47,7 +34,7 @@ app.get('/bestSell', (req, res) =>{
             const bestSellproduct1 = result[0];
             const bestSellproduct2 = result[1];
             const bestSellproduct3 = result[2];
-            //console.log(bestSellproduct3);
+            // console.log(bestSellproduct3);
             res.status(201).json({product1: bestSellproduct1,product2: bestSellproduct2,product3: bestSellproduct3})
         }
     });
@@ -79,10 +66,8 @@ app.get('/product_cart', (req, res) =>{
 
 app.post('/user_product', (req, res) => {
     const userid = req.body.userid;
-    db.query(
-    "SELECT cart.cartid, product.name, product_history.quantity, product_history.totalprice, cart.payment_status FROM cart JOIN product_history ON product_history.cartid = cart.cartid JOIN product ON product.productid = product_history.productid WHERE userid = ? ORDER BY cart.cartid",
-    [userid],
-    (err, result) => {
+    db.query("SELECT cartid, payment_status FROM cart WHERE userid = ? ORDER BY cartid",
+    [userid],   (err, result) => {
         if(err){
             console.log(err);
         }
@@ -91,6 +76,21 @@ app.post('/user_product', (req, res) => {
         }
     });
 });
+
+app.post('/reqproductincart', (req, res) => {
+    const cartid = req.body.cartid;
+    db.query("SELECT product.productid, product.name, product_history.quantity, totalprice FROM product_history JOIN product ON product_history.productid = product.productid WHERE cartid = ? ORDER BY id",
+    [cartid], (err, result) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.send(result);
+            //console.log(result);
+        }
+    });
+});
+
 
 app.post('/signup', async (req, res) =>{
     const email = req.body.email;
@@ -229,6 +229,133 @@ app.get('/getproductSelected', (req,res) => {
         }
     }); 
 });
+
+const min = 1;
+const max = 100;
+const randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+app.post('/postproductid', (req, res) => {
+    const productid = req.body.productid;
+    const userid = req.body.userid;
+    const quantity = req.body.quantity;
+    const price = req.body.price;
+
+    db.query("SELECT cartid , payment_status FROM cart WHERE userid = ? AND payment_status = 'not_submit'", [userid], (err, result) => {
+        if(err){
+            console.log(err);
+            res.status(500).json({ message: "Error database !"});
+        }
+        else{
+            // console.log(result);
+            if(result.length === 0){
+                // console.log(result)
+                db.query("INSERT INTO cart (payment_status, qr_picture, userid) VALUES (?, ?, ?)",
+                    ["not_submit", "/qrimage"+randomInt, userid], (err, result) => {
+                        if(err){
+                            console.log(err);
+                            res.status(500).json({message: "Error Inserted cart to database!"});
+                        }
+                        else{
+                            // console.log(result);
+                        }
+                    });
+                db.query("SELECT cartid , payment_status FROM cart WHERE userid = ? AND payment_status = 'not_submit'",
+                [userid], (err, result) => {
+                    if(err){
+                        console.log(err);
+                        res.status(500).json({ message: "Error database !"});
+                    }
+                    else{
+                        // console.log(result[0].cartid);
+                        let cartid = result[0].cartid;
+                        db.query("INSERT INTO product_history (productid, quantity, totalprice, cartid) VALUES (?, ?, ?, ?)",
+                            [productid, quantity, price*quantity, cartid], (err, result) => {
+                            if(err){
+                                console.log(err);
+                                    res.status(500).json({message: "Error Inserted product to database!"});
+                                }
+                                else{
+                                    // console.log(result);
+                                    // res.status(200).json({message: "Inserted product Success"});
+                                }
+                            });
+                    }
+                });
+            }
+            else{
+                let cartid = result[0].cartid;
+                // console.log(result[0].cartid)
+                db.query("INSERT INTO product_history (productid, quantity, totalprice, cartid) VALUES (?, ?, ?, ?)",
+                    [productid, quantity, price*quantity, cartid], (err, result) => {
+                    if(err){
+                        console.log(err);
+                            res.status(500).json({message: "Error Inserted product to database!"});
+                        }
+                        else{
+                            // console.log(result);
+                            // res.status(200).json({message: "Inserted product Success"});
+                        }
+                    });
+            }
+            res.status(200).json({ message: "Inserted product Success!" });
+        }
+    });
+});
+
+app.post('/getcart', (req, res) => {
+    const userid = req.body.userid;
+    db.query("SELECT cartid FROM cart WHERE userid = ? AND payment_status = 'not_submit'",
+    [userid], (err, result) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            // console.log(result[0].cartid);
+            // res.send(result);
+            const cartid = result[0].cartid;
+            db.query(
+            "SELECT product_history.id, product_history.productid, product.name, product_history.quantity, totalprice FROM product_history JOIN product ON product.productid = product_history.productid WHERE product_history.cartid = ? ORDER BY product_history.productid",
+            [cartid], (err, result) => {
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    for(let i=0 ; i<result.length ; i++){
+                        result[i].cartid = cartid;
+                    }
+                    res.send(result);
+                    // console.log(result);
+                }
+            })            
+        }
+    });
+});
+
+app.get('/getAllproduct', (req, res) => {
+    db.query("SELECT * FROM product", (err, result) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.send(result);
+        }
+    })
+});
+
+app.post('/deleteProduct', (req, res) => {
+    const productid = req.body.productid;
+    const cartid = req.body.cartid;
+    const id = req.body.id;
+    db.query("DELETE FROM product_history WHERE productid = ? AND cartid = ? AND id = ?", 
+    [productid, cartid, id],
+    (err, result) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.status(200).json({ message: "Delete Successed!" });
+        }
+    });
+}); 
 
 app.listen('3001', () =>{
     console.log('Server is running on port 3001');
